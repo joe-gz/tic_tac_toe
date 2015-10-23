a7fe59c0ed2a2d60b04e27f71c49581cb7226f9c
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-23 14:34:07 -0400

    finished determine winner

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var player1 = "player X";
 var player2 = "player O";
+indexArray=[0,1,2,3,4,5,6,7,8];
 
   $("header div").text("Player turn: "+player1)
 
@@ -7,17 +8,22 @@ $(".square").each(function (){
   var clickCount = 0;
   $(this).on("click",function(){
     if (clickCount === 0 ){
+      indexArray[$(this).index()] = player1;
       $(this).css("background","green");
       $(this).append("<p>X</p>")
       clickCount++
+      checkForWinner();
       $("header div").text("Player turn: "+player2)
 
     } else if (clickCount === 1){
+      indexArray[$(this).index()] = player2;
       $(this).css("background","purple");
       $(this, "p").html("<p>O</p>")
       clickCount++
+      checkForWinner();
       $("header div").text("Player turn: "+player1)
     } else {
+      indexArray[$(this).index()] = ""
       $(this, "p").text(" ");
       $(this).css("background","blue");
       clickCount = 0;
@@ -39,3 +45,40 @@ $("button").eq(0).on("click", function () {
   console.log(player1);
   console.log(player2);
 })
+
+function checkForWinner(){
+  if (indexArray[0]===indexArray[1] && indexArray[0]=== indexArray[2]) {
+    var winner = indexArray[0];
+    console.log("Winner is: ", winner);
+    $("main").prepend("<div class= 'win'></div>");
+    $(".win").html("Winner is: "+ winner);
+  } else if (indexArray[3]===indexArray[4] && indexArray[3]=== indexArray[5]) {
+    var winner = indexArray[3];
+    $("main").prepend("<div class= 'win'></div>");
+    $(".win").html("Winner is: "+ winner);
+  } else if (indexArray[6]===indexArray[7] && indexArray[6]=== indexArray[8]) {
+    var winner = indexArray[6];
+    $("main").prepend("<div class= 'win'></div>");
+    $(".win").html("Winner is: "+ winner);
+  } else if (indexArray[0]===indexArray[3] && indexArray[0]=== indexArray[6]) {
+    var winner = indexArray[0];
+    $("main").prepend("<div class= 'win'></div>");
+    $(".win").html("Winner is: "+ winner);
+  } else if (indexArray[1]===indexArray[4] && indexArray[1]=== indexArray[7]) {
+    var winner = indexArray[1];
+    $("main").prepend("<div class= 'win'></div>");
+    $(".win").html("Winner is: "+ winner);
+  } else if (indexArray[2]===indexArray[5] && indexArray[2]=== indexArray[8]) {
+    var winner = indexArray[2];
+    $("main").prepend("<div class= 'win'></div>");
+    $(".win").html("Winner is: "+ winner);
+  } else if (indexArray[0]===indexArray[4] && indexArray[0]=== indexArray[8]) {
+    var winner = indexArray[0];
+    $("main").prepend("<div class= 'win'></div>");
+    $(".win").html("Winner is: "+ winner);
+  } else if (indexArray[2]===indexArray[4] && indexArray[2]=== indexArray[6]) {
+    var winner = indexArray[2];
+    $("main").prepend("<div class= 'win'></div>");
+    $(".win").html("Winner is: "+ winner);
+  }
+}
